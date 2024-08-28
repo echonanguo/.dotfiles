17e946e7e3b3b44e8a41dbc7424c0928ed25cf92
nanguo <nanguo@localhost.localdomain>
2024-08-28 09:06:05 +0800

    2024_8_28

--- tag-nvim/config/nvim/lua/core/keymaps.lua
+++ tag-nvim/config/nvim/lua/core/keymaps.lua
@@ -19,18 +19,38 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
--- 切换buffer
-keymap.set("n", "<C-L>", ":bnext<CR>")
-keymap.set("n", "<C-H>", ":bprevious<CR>")
+-- buffer
+keymap.set("n", "<leader>l", ":bnext<CR>")
+keymap.set("n", "<leader>h", ":bprevious<CR>")
+keymap.set("n", "<leader>ts", ":BufferLineCloseOthers<CR>")
+
+-- 开发
+keymap.set("n","<Leader>r",":!~/.scripts/run.sh %<CR>")
+keymap.set("n","<Leader>e",":!~/.scripts/execute.sh %<CR>")
+keymap.set("n","<Leader>c",":!~/.scripts/compile.sh %<CR>")
+
+
+-- 取消 LSP 默认的 K 映射
+keymap.set('n', 'K', '')
+
+-- 将 LSP 悬停功能映射到 F
+keymap.set('n', 'F', vim.lsp.buf.hover, { noremap = true, silent = true })
+
+-- Shift + HJKL 快速移动光标
+vim.api.nvim_set_keymap('n', '<S-H>', '5h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-J>', '5j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-K>', '5k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-L>', '5l', { noremap = true, silent = true })
 
 -- ---------- 插件 ---------- ---
+
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>g", ":NvimTreeToggle<CR>")
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})  -- 查找文件
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})  -- 全局查找
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})  -- 查找打开的 buffer
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})  -- 查找 help 标签
+keymap.set('n', '<leader>ff', builtin.find_files, {})  -- 查找文件
+keymap.set('n', '<leader>fg', builtin.live_grep, {})  -- 全局查找
+keymap.set('n', '<leader>fb', builtin.buffers, {})  -- 查找打开的 buffer
+keymap.set('n', '<leader>fh', builtin.help_tags, {})  -- 查找 help 标签
 
--- tag-nvim/config/nvim/lua/core/options.lua
+++ tag-nvim/config/nvim/lua/core/options.lua
@@ -30,3 +30,24 @@ opt.smartcase = true
 -- 外观
 opt.termguicolors = true
 opt.signcolumn = "yes"
+
+-- 使用基于缩进的折叠
+opt.foldmethod = "indent"
+
+
+
+-- 启用 viminfo 功能以保存和恢复光标位置
+opt.viminfo:append("'100") -- 保存最近 100 个文件的光标位置
+opt.viminfo:append("n")    -- 保存标记
+opt.viminfo:append("f")    -- 保存文件名
+opt.viminfo:append("s")    -- 保存搜索历史
+-- 自动恢复光标位置
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line("'\"")
+    if line > 0 and line <= vim.fn.line("$") then
+      vim.cmd("normal! g`\"")
+    end
+  end
+})
