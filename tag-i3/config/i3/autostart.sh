4ee33f5fc95fe0b4a068b2ae8bd3d6aad0b46348
nanguo <zhuyuqinss@gmail.com>
2024-12-12 23:38:36 +0800

    2024-12-12

---
+++ tag-i3/config/i3/autostart.sh
@@ -0,0 +1,12 @@
+# redshift
+redshift &
+
+# picom
+picom --config ~/.config/picom/picom.conf1 &
+
+# fcitx5
+fcitx5 &
+
+# feh
+feh --bg-scale https://w.wallhaven.cc/full/md/wallhaven-mdpgl8.png &
+
