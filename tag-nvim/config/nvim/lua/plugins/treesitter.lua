ff24d598a4d001d517aa61a211718a2e7f4a69cb
nanguo <zhuyuqinss@gmail.com>
2024-12-15 22:55:10 +0800

    2024-12-15

--- tag-nvim/config/nvim/lua/plugins/treesitter.lua
+++ tag-nvim/config/nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,16 @@ return {
     ensure_installed = {
       "lua",
       "vim",
-      -- add more arguments for adding more treesitter parsers
+      "html", -- HTML 支持
+      "css", -- CSS 支持
+      "javascript", -- JavaScript 支持
+      "typescript", -- TypeScript 支持
+      "json", -- JSON 支持
+      "scss", -- SCSS 支持
+      "jsonc", -- JSONC 支持（带注释的 JSON）
+      "markdown", -- Markdown 支持
+      "tsx", -- TSX (TypeScript + JSX) 支持
+      "jsx", -- JSX 支持
     },
   },
 }
