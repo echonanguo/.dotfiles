7abe001d6cb535a8b61207860c6258c0cef7a807
nanguo <zhuyuqinss@gmail.com>
2024-12-16 22:51:09 +0800

    最初的模样

--- tag-nvim/config/nvim/lua/plugins/astrocore.lua
+++ tag-nvim/config/nvim/lua/plugins/astrocore.lua
@@ -30,6 +30,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = true, -- 软换行
+        scrolloff = 25, -- 提高 scrolloff，避免光标过于贴近边界
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
