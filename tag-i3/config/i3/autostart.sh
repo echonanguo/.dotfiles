b25c06da912980e6171fc61984f1941240ae4f2b
zhuyuqin <echonanguo@qq.com>
2025-07-20 21:35:58 +0800

    2025-7-20

--- tag-i3/config/i3/autostart.sh
+++ tag-i3/config/i3/autostart.sh
@@ -1,11 +1,11 @@
 # redshift
-redshift &
+#redshift &
 
 # picom
 picom --config ~/.config/picom/picom.conf1 &
 
 # fcitx5
-fcitx5 &
+#fcitx5 &
 
 # feh
 feh --bg-scale /home/nanguo/.config/i3/wallhaven-mdpgl8.png &
--- tag-nvim/config/nvim/lua/plugins/lsp_signature.lua
+++
@@ -1,6 +0,0 @@
--- lsp增强
-return {
-  "ray-x/lsp_signature.nvim",
-  event = "BufRead",
-  config = function() require("lsp_signature").setup() end,
-}
